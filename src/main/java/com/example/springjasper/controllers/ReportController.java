0a3168935a6a4ffb988badf879496152185bb1fe
Carlos Silva <miguel21.melo21@gmail.com>
2023-09-07 11:31:48 -0300

    initial commit

---
+++ src/main/java/com/example/springjasper/controllers/ReportController.java
@@ -0,0 +1,43 @@
+package com.example.springjasper.controllers;
+
+import com.example.springjasper.models.ReportModel;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ReportController {
+//    @CrossOrigin(origins = "*")
+    @PostMapping("/report")
+    public ResponseEntity<byte[]> createReport(@RequestBody List<ReportModel> reports) throws FileNotFoundException, JRException {
+        Map<String, Object> params = new HashMap<>();
+        File file = ResourceUtils.getFile("classpath:reports/base.jasper");
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(reports);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(file.getAbsolutePath(), params, dataSource);
+
+        ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
+        JRPdfExporter pdfExporter = new JRPdfExporter();
+        pdfExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        pdfExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(pdfStream));
+        pdfExporter.exportReport();
+
+        return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_PDF).body(pdfStream.toByteArray());
+    }
+}
