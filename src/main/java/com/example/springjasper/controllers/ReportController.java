f7894c98e64137e2399ad21ca488650f4f54117d
Carlos Silva <miguel21.melo21@gmail.com>
2023-09-08 09:39:34 -0300

    class path resource input stream

--- src/main/java/com/example/springjasper/controllers/ReportController.java
+++ src/main/java/com/example/springjasper/controllers/ReportController.java
@@ -6,15 +6,13 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
 import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,14 +20,15 @@ import java.util.Map;
 @RestController
 public class ReportController {
     @GetMapping("/")
-    public String hello() { return "Hello World!"; };
+    public String hello() { return "Hello World!"; }
 
     @PostMapping("/report")
-    public ResponseEntity<byte[]> createReport(@RequestBody List<ReportModel> reports) throws FileNotFoundException, JRException {
+    public ResponseEntity<byte[]> createReport(@RequestBody List<ReportModel> reports) throws IOException, JRException {
         Map<String, Object> params = new HashMap<>();
-        File file = ResourceUtils.getFile("classpath:reports/base.jasper");
+        ClassPathResource file = new ClassPathResource("reports/base.jasper");
+
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(reports);
-        JasperPrint jasperPrint = JasperFillManager.fillReport(file.getAbsolutePath(), params, dataSource);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(file.getInputStream(), params, dataSource);
 
         ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
         JRPdfExporter pdfExporter = new JRPdfExporter();
